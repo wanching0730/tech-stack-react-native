fdb28a03c14ffafe43db6c65ebdf6be5ab3b6af3
wanching_97 <wanching_97@hotmail.com>
2018-05-22 22:40:32 +0800

    setup second reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import LibraryReducer from './LibraryReducer';
+import SelectionReducer from './SelectionReducer';
 
 export default combineReducers({
     // store the state returned by LibraryReducer
-    libraries: LibraryReducer
+    libraries: LibraryReducer,
+    selectedLibraryId: SelectionReducer
 });
