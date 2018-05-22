b66641918fb1ac3540bd0ffc8c96e46c96c7c1fe
wanching_97 <wanching_97@hotmail.com>
2018-05-22 19:51:54 +0800

    setup ListView

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { ListView } from 'react-native';
 
 class LibraryList extends Component {
+
+    componentWillMount() {
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 != r2
+        });
+
+        this.dataSource = ds.cloneWithRows(this.props.libraries);
+    }
+
+    renderRow() {
+        
+    }
+
     render() {
-        console.log(this.props);
-        return;
+        return (
+            <ListView
+                dataSource={this.dataSource}
+                renderRow={this.renderRow}
+            />
+        )
     }
 }
 
+// take the application state to be props of LibraryList component, which is 'libraries'
 const mapStateToProps = state => {
     return { libraries: state.libraries };
 };
