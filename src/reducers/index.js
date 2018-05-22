980c8f6b3c6b6ee5f8c2d8f580cf7ec8bbda062b
wanching_97 <wanching_97@hotmail.com>
2018-05-22 16:12:48 +0800

    update state of libraris as library json

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,5 +2,6 @@ import { combineReducers } from 'redux';
 import LibraryReducer from './LibraryReducer';
 
 export default combineReducers({
+    // store the state returned by LibraryReducer
     libraries: () => LibraryReducer
 });
