b4ab2d1218e98148a4a07df6701216b0a792a917
wanching_97 <wanching_97@hotmail.com>
2018-05-23 22:19:49 +0800

    added expanded variable

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -10,9 +10,9 @@ import * as actions from '../actions';
 class ListItem extends Component {
 
     renderDescription() {
-        const { library, selectedLibraryId } = this.props;
+        const { library, expanded } = this.props;
 
-        if (library.id === selectedLibraryId) {
+        if (expanded) {
             return (
                 <Text>{library.description}</Text>
             );
@@ -47,8 +47,11 @@ const styles = {
     }
 };
 
-const mapStateToProps = state => {
-    return { selectedLibraryId: state.selectedLibraryId};
+const mapStateToProps = (state, ownProps) => {
+    const expanded = state.selectedLibraryId === ownProps.library.id;
+
+    // return { expanded: expanded };
+    return { expanded };
 }
 
 // first argument is mapStateToProps
